8de25487dc0d6f3276f2fd1f59a3c9aeefe5bb84
Santiago Burbano <sburbano@itworks.ec>
2012-11-22 22:44:34 -0500

    robust asset downloader now handles doctor and service assets

---
+++ libs/s3AssetDownloader.js
@@ -0,0 +1,120 @@
+var
+    fs = require('fs'),
+    path = require("path"),
+    async = require('async'),
+    http = require('http'),
+    MapModel = require('../models/map'),
+    DoctorModel = require('../models/doctor'),
+    ServiceModel = require('../models/service');
+
+var
+    S3ROOT = 'https//s3.amazonaws.com/itworks.ec/mapeditor/images/',
+    downloadDirectory = 'tmp',
+    arAssetList = new Array();
+
+var fnProcessGenericCollection = function(callback, Model, arImageFields) {
+    Model.find(function (err, entries) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        if (!entries ||  entries.length == 0){
+            callback();
+            return;
+        }
+
+        // loop models
+        for (var i in entries) {
+            var item = entries[i];
+
+            // loop imageFields
+            for (var j in arImageFields) {
+                var fieldName = arImageFields[j];
+                if (item[fieldName]) {
+                    arAssetList.push(item[fieldName]);
+                }
+            }
+        }
+        callback();
+    });
+}
+
+// Function to download file
+var downloadFile = function(storedPath, callback) {
+
+    // obtain final path and create if it does not exist!
+    var fullPath = path.join(downloadDirectory, storedPath);
+    var fileUrl = path.join(S3ROOT, storedPath);
+
+    // targetDir
+    var targetDir = path.dirname(fullPath);
+    if (!fs.existsSync(targetDir)) {
+        fs.mkdirSync(targetDir);
+    }
+
+    console.log('full-path', fullPath);
+    console.log('fileUrl', fileUrl);
+
+    // if file exists, delete it
+    if (fs.existsSync(fullPath)){
+        var err = fs.unlinkSync(fullPath);
+        if (err){
+            return callback(err);
+        }
+    }
+
+    var file = fs.createWriteStream(fullPath);
+    http.get(fileUrl, function(res) {
+        res.on('data', function(data) {
+            file.write(data);})
+            .on('end', function() {
+                console.log('DOWNLOADED: ' + fileUrl);
+                file.end();
+                callback();
+            })
+            .on('error', function(e) {
+                console.log("ERROR: " + e.message);
+                file.end();
+                callback(e);
+            });
+    })
+};
+
+var fnProcessMaps = function(callback) {
+    fnProcessGenericCollection(callback, MapModel, ['imageName', 'linkImageName']);
+}
+
+var fnProcessServices = function(callback) {
+    fnProcessGenericCollection(callback, ServiceModel, ['imageUrl']);
+}
+
+var fnProcessDoctors = function(callback) {
+    fnProcessGenericCollection(callback, DoctorModel, ['imageUrl']);
+}
+
+module.exports = {
+
+    downloadAssets : function(mainCallback) {
+        // reset
+        arAssetList.length = 0;
+
+        // make sure directory exists
+        downloadDirectory = path.join(process.env.mapEditorRoot, '/public/data/images');
+        if (!fs.existsSync(downloadDirectory)) {
+            fs.mkdirSync(downloadDirectory);
+        }
+        // get list of all used assets
+        async.parallel([fnProcessMaps, fnProcessServices, fnProcessDoctors], function(err, results){
+            console.log('arAssetList', arAssetList);
+
+            // copy locally
+            async.forEach(arAssetList, function(item, callback) {
+                downloadFile(item, callback);
+            }, function(err){
+                mainCallback(err);
+            });
+        });
+
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,47 +1,5 @@
-var MapModel = require('../models/map');
-
-// Dependencies
 var
-    url = require('url'),
-    exec = require('child_process').exec,
-    fs = require('fs'),
-    path = require("path"),
-    async = require('async'),
-    http = require('http');
-
-// App variables
-var S3ROOT = 'https//s3.amazonaws.com/itworks.ec/mapeditor/images/';
-
-// Function to download file
-var download_file = function(DOWNLOAD_DIR, file_url, callback) {
-
-    var file_name = url.parse(file_url).pathname.split('/').pop();
-
-    var fullName = path.join(DOWNLOAD_DIR, file_name);
-    // if file exists, delete it
-    if (fs.existsSync(fullName)){
-        var err = fs.unlinkSync(fullName);
-        if (err){
-            return callback(err);
-        }
-    }
-
-    var file = fs.createWriteStream(fullName);
-    http.get(file_url, function(res) {
-        res.on('data', function(data) {
-            file.write(data);})
-            .on('end', function() {
-                console.log('FINISHED: ' + file_url);
-                file.end();
-                callback();
-            })
-            .on('error', function(e) {
-                console.log("ERROR: " + e.message);
-                file.end();
-                callback(e);
-            });
-    })
-};
+    s3AssetDownloader = require('../libs/s3AssetDownloader');
 
 module.exports = {
     index : function(req, res) {
@@ -54,35 +12,9 @@ module.exports = {
 
     localSetupPost : function(req, res, next) {
 
-        var
-            downloadPath = path.join(process.env.mapEditorRoot, '/public/data/images');
-        // make sure directory exists
-        if (!fs.existsSync(downloadPath)) {
-            fs.mkdirSync(downloadPath);
-        }
-
-        // loop through maps
-        var arClonedImages = new Array();
-
-        MapModel.find(function (err, entries) {
-            if (!err) {
-                if (entries && entries.length > 0)
-                {
-                    async.forEach(entries, function(item, callback){
-                        arClonedImages.push(item.imageName);
-                        download_file(downloadPath, S3ROOT + item.imageName, callback);
-                    }, function(err){
-                        console.log(arClonedImages);
-                        if (err) {
-                            return res.send(err);
-                        }
-                        res.redirect('/admin/localSetupSuccess');
-                    });
-                }
-            }
-            else {
-                return next(err);
-            }
+        s3AssetDownloader.downloadAssets(function(err){
+            // TODO: handle error
+            res.redirect('/admin/localSetupSuccess');
         });
     },
 
